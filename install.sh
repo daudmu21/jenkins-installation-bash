a6f955489d1fc5d982cd3411d0dd3d16e9896c66
azaa1 <azaa.chi11@gmail.com>
2019-05-09 15:05:28 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install 'curl' and 'rpm'
+sudo yum install curl rpm -y
+
+#Install OpenJDK 8
+sudo yum install java-1.8.0-openjdk-devel -y
+
+#Enable Jenkins Repository
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+#Install Jenkins
+sudo yum install jenkins -y
+
+#Start Jenkins
+sudo systemctl start jenkins
+
+#Enable Jenkins
+sudo systemctl enable jenkins
